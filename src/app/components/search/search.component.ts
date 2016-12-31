cc117553d9871cca43627ca2790a6eff0cf4ff3e
IHSAN SENGUN <ihsan.sengun@kaplan.com>
2016-12-31 01:49:04 +0000

    albums has been added

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,26 @@
+import {Component} from "@angular/core";
+import {SpotifyService} from "../../services/spotify.service";
+import {Artist} from '../../artist';
+
+
+@Component({
+    selector: 'app-search',
+    templateUrl: './search.component.html'
+})
+export class SearchComponent {
+    searchStr: string;
+    searchRes:Artist[];
+
+
+    constructor(private _spotifyService: SpotifyService) {
+
+    }
+
+    searchMusic() {
+        this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
+            this.searchRes = res.artists.items;
+        })
+
+    }
+
+}
